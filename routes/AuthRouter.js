6b0e367594694241bd76b07a4d41a3f05a6f13a9
Nell Flores <nell.djmf@gmail.com>
2022-05-08 22:51:01 -0400

    created server entry point, empty route files and controller files

---
+++ routes/AuthRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const controller = require('../controllers/AuthController')
+const middleware = require('../middleware')
+
+router.post('/login', controller.Login)
+router.post('/register', controller.Register)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+router.post(
+    '/update',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdatePassword
+)
+
+module.exports = router
